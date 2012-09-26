aedfb629133258cf45e60a7526fdc6e6ba5ad8af
Filipe Carvalho <filipemabpcarvalho@gmail.com>
2012-09-26 15:53:34 +0100

    prints out

--- mads1/src/main.java
+++ mads1/src/main.java
@@ -6,11 +6,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("MADS"); //a ver se o travis funciona....
-		System.out.println("Commit Joana");
-		System.out.println("Commit Pedro");
-		System.out.println("Commit Filipe");
-		System.out.println("Commit gitignore");
+		
 	}
 
 }
